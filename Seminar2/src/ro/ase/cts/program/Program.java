119916e40c1cf1f2a418f2becceef93a61df2767
mariusburlacu <marius.bm29@gmail.com>
2021-03-04 11:24:22 +0200

    seminar2 - final

--- Seminar2/src/ro/ase/cts/program/Program.java
+++ Seminar2/src/ro/ase/cts/program/Program.java
@@ -1,19 +1,27 @@
 package ro.ase.cts.program;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.List;
 
 import ro.ase.cts.clase.Angajat;
+import ro.ase.cts.clase.Aplicant;
 import ro.ase.cts.readers.AngajatReader;
+import ro.ase.cts.readers.AplicantReader;
+import ro.ase.cts.readers.StudentReader;
 
 public class Program {
+	
+	public static List<Aplicant> readAplicant(String numeFisier, AplicantReader reader) throws NumberFormatException, FileNotFoundException{
+		return reader.readAplicants(numeFisier);
+	}
 
 	public static void main(String[] args) {
-		List<Angajat> listaAngajati;
+		List<Aplicant> listaAplicanti;
 		try {
-			listaAngajati = AngajatReader.readAngajati("angajati.txt");
-			for(Angajat angajat:listaAngajati)
-				System.out.println(angajat.toString());
+			listaAplicanti = readAplicant("studenti.txt", new StudentReader());
+			for(Aplicant aplicant:listaAplicanti)
+				System.out.println(aplicant.toString());
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
