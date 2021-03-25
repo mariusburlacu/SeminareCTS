893e1c226dcf20ba3402edc1f4331a9ca774dd12
mariusburlacu <marius.bm29@gmail.com>
2021-03-25 11:21:33 +0200

    seminar 5 - simple + factory + prototype

---
+++ Seminar5Factory/src/ro/ase/cts/prototype/Reteta.java
@@ -0,0 +1,40 @@
+package ro.ase.cts.prototype;
+
+public class Reteta implements RetetaMedicament {
+	private String solutie;
+	private int cantitate;
+	
+	public Reteta() {
+		super();
+	}
+
+	public Reteta(String solutie, int cantitate) {
+		super();
+		this.solutie = solutie;
+		this.cantitate = cantitate;
+	}
+
+	public String getSolutie() {
+		return solutie;
+	}
+
+	public int getCantitate() {
+		return cantitate;
+	}
+
+	@Override
+	public String toString() {
+		return "Reteta [solutie=" + solutie + ", cantitate=" + cantitate + "]";
+	}
+
+	@Override
+	public RetetaMedicament copiaza() {
+		Reteta reteta = new Reteta();
+		reteta.solutie = this.solutie;
+		reteta.cantitate = this.cantitate;
+		
+		return reteta;
+	}
+	
+	
+}
