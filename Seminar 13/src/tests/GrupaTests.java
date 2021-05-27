edafecf7a09e2f97637d69962073cedb24574fd6
mariusburlacu <marius.bm29@gmail.com>
2021-05-27 11:20:21 +0300

    Seminar 13 - JUNIT - Right-BICEP + CORRECT

--- Seminar 13/src/tests/GrupaTests.java
+++ Seminar 13/src/tests/GrupaTests.java
@@ -2,11 +2,77 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import clase.Grupa;
+import clase.Student;
 
 public class GrupaTests {
+	private Grupa grupa;
+	
+	@Before
+	public void setUp(){
+		grupa = new Grupa(1077);
+		for(int i=0; i<10; i++) {
+			Student student = new Student("Marcel");
+			student.adaugaNota(5);
+			student.adaugaNota(4);
+			student.adaugaNota(10);
+			grupa.adaugaStudent(student);
+		}
+	}
+	
+	// TESTE PENTRU METODA GET PROMOVABILITATE
+	
+	@Test
+	public void testGetPromovabilitate() {
+		Student student = new Student("Marcel");
+		student.adaugaNota(10);
+		student.adaugaNota(10);
+		
+		Student studentMaria = new Student("Maria");
+		studentMaria.adaugaNota(10);
+		studentMaria.adaugaNota(10);
+		
+		grupa.adaugaStudent(studentMaria);
+		grupa.adaugaStudent(student);
+		
+		assertEquals(0.16, grupa.getPromovabilitate(), 0.05);
+	}
+	
+	@Test
+	public void testGetPromovabilitateLowerBoundary() {
+		assertEquals(0,  grupa.getPromovabilitate(), 0.05);
+	}
+	
+	@Test
+	public void testGetPromovabilitateUpperBoundary() {
+		Grupa grupa = new Grupa(1077);
+		
+		for(int i=0; i<10; i++) {
+			Student student = new Student("Ion");
+			student.adaugaNota(5);
+			student.adaugaNota(6);
+			student.adaugaNota(10);
+			grupa.adaugaStudent(student);
+		}
+		
+		assertEquals(1,  grupa.getPromovabilitate(), 0.05);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testGetPromovabilitateError() {
+		Grupa grupaNoua = new Grupa(1032);
+		grupaNoua.getPromovabilitate();
+	}
+
+	@Test(timeout = 500)
+	public void testGetPromovabilitateTime() {
+		grupa.getPromovabilitate();
+	}
+
+	// TESTE PENTRU CONSTRUCTORUL GRUPA
 
 	@Test
 	public void testConstructorRight() {
@@ -49,4 +115,6 @@ public class GrupaTests {
 		Grupa grupa = new Grupa(1077);
 		assertNotNull(grupa.getStudenti());
 	}
+	
+	
 }
